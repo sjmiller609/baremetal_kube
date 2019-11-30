d2b8b8114ff2be5b00520a777616dd52fa9ab810
Steven Miller <sjmiller609@gmail.com>
2019-11-29 22:38:13 -0500

    Fix etcd count in inventory

--- inventory/inventory.py
+++ inventory/inventory.py
@@ -38,9 +38,9 @@ class LocalNetworkInventory(object):
 
         self.hostvars = \
             {
-                "localhost": {
-                    "ansible_connection": "local",
-                    "ansible_python_interpreter": "/usr/bin/python"
+                'localhost': {
+                    'ansible_connection': 'local',
+                    'ansible_python_interpreter': '/usr/bin/python'
                 }
             }
 
@@ -61,23 +61,23 @@ class LocalNetworkInventory(object):
             stdin, stdout, stderr = client.exec_command('cat /etc/motd')
             return stdout.read().decode('utf-8')
         except Exception as e:
-            sys.stderr.write(f"{e}")
+            sys.stderr.write(f'{e}')
         finally:
             client.close()
-        return ""
+        return ''
 
     def _detect_os(self, host):
         motd = self._get_motd(host,
                               port=22,
                               username=self.USERNAME,
                               password=self.PASSWORD)
-        if "edgeos" in motd.lower():
-            return "edgeos"
+        if 'edgeos' in motd.lower():
+            return 'edgeos'
         motd = self._get_motd(host,
                               port=22,
-                              username="core")
-        if "container linux" in motd.lower():
-            return "container_linux"
+                              username='core')
+        if 'container linux' in motd.lower():
+            return 'container_linux'
         return None
 
     def _discover_ssh_connect(self):
@@ -87,26 +87,34 @@ class LocalNetworkInventory(object):
 
         for host in self.ssh_hosts:
             os = self._detect_os(host)
-            if os == "edgeos":
-                sys.stderr.write(f"Detected EdgeOS on host: {host}\n")
+            if os == 'edgeos':
+                sys.stderr.write(f'Detected EdgeOS on host: {host}\n')
                 self.routers.append(host)
-            elif os == "container_linux":
-                sys.stderr.write(f"Detected Container Linux on host: {host}\n")
-                hostname = f"node{len(self.container_linux)}"
+            elif os == 'container_linux':
+                sys.stderr.write(f'Detected Container Linux on host: {host}\n')
+                hostname = f'node{len(self.container_linux)}'
                 self.container_linux.append(hostname)
-                self.hostvars[hostname] = {"ansible_host":host}
+                self.hostvars[hostname] = {
+                    'ansible_host': host,
+                    'ansible_user': 'core',
+                    'ansible_ssh_private_key_file': '~/.ssh/id_rsa',
+                    'ansible_python_interpreter': '/opt/bin/python',
+                    'bin_dir': '/opt/bin',
+                    'ansible_become': 'yes'
+                }
+
             else:
-                sys.stderr.write(f"Did not detect Container Linux or EdgeOS on host: {host}\n")
+                sys.stderr.write(f'Did not detect Container Linux or EdgeOS on host: {host}\n')
 
         if len(self.routers) > 1:
             raise Exception(
-                "Currently, only a 1 router configuration is supported. "
-                f"Found {self.routers}")
+                'Currently, only a 1 router configuration is supported. '
+                f'Found {self.routers}')
 
     def _discover_ssh_open(self):
 
         for cidr in self.cidrs:
-            sys.stderr.write(f"Scanning port 22 in network {cidr}\n")
+            sys.stderr.write(f'Scanning port 22 in network {cidr}\n')
             self.nm.scan(hosts=cidr, ports='22', arguments='-n', sudo=False)
 
         self.ssh_hosts = []
@@ -119,19 +127,19 @@ class LocalNetworkInventory(object):
     def _discover_cidr(self):
         self.cidrs = []
         interfaces = netifaces.interfaces()
-        interfaces = list(filter(lambda x: x[0] == "e", interfaces))
+        interfaces = list(filter(lambda x: x[0] == 'e', interfaces))
 
         if len(interfaces) < 1:
-            raise Exception("Did not detect any interfaces starting "
+            raise Exception('Did not detect any interfaces starting '
                             "with the character 'e'")
 
         for interface in interfaces:
-            sys.stderr.write(f"Detected interface {interface}:\n")
+            sys.stderr.write(f'Detected interface {interface}:\n')
 
             if not netifaces.AF_INET in netifaces.ifaddresses(interface):
-                sys.stderr.write(f"Interface {interface} does not have a configured IP address. Configuring to the default network for EdgeLITE router:\n")
-                check_output(["ifconfig", interface, "192.168.1.100"])
-                check_output(["ifconfig", interface, "netmask", "255.255.255.0"])
+                sys.stderr.write(f'Interface {interface} does not have a configured IP address. Configuring to the default network for EdgeLITE router:\n')
+                check_output(['ifconfig', interface, '192.168.1.100'])
+                check_output(['ifconfig', interface, 'netmask', '255.255.255.0'])
 
             for address in netifaces.ifaddresses(interface)[netifaces.AF_INET]:
                 ip_address = address['addr']
@@ -139,17 +147,17 @@ class LocalNetworkInventory(object):
 
                 if not ipaddress.ip_address(ip_address).version == 4:
                     sys.stderr.write(
-                        f"Ignoring non-IPv4 address: {ip_address}\n")
+                        f'Ignoring non-IPv4 address: {ip_address}\n')
 
                     continue
-                remove_host_bits = ip_address.split(".")
+                remove_host_bits = ip_address.split('.')
                 remove_host_bits.pop()
-                remove_host_bits.append("0")
-                remove_host_bits = ".".join(remove_host_bits)
+                remove_host_bits.append('0')
+                remove_host_bits = '.'.join(remove_host_bits)
                 netmask = ipaddress.IPv4Network(
-                    f"{remove_host_bits}/{netmask}").prefixlen
-                cidr = f"{remove_host_bits}/{netmask}"
-                sys.stderr.write(f"Detected cidr {cidr}\n")
+                    f'{remove_host_bits}/{netmask}').prefixlen
+                cidr = f'{remove_host_bits}/{netmask}'
+                sys.stderr.write(f'Detected cidr {cidr}\n')
                 self.cidrs.append(cidr)
 
     def _build_inventory(self):
@@ -157,46 +165,43 @@ class LocalNetworkInventory(object):
             'all': {
                 'children': ['routers', 'container_linux', 'pxe_servers']
             },
-            "routers": {
-                "hosts": self.routers,
-                "vars": {
-                    "ansible_user": self.USERNAME,
-                    "ansible_password": self.PASSWORD,
-                    "ansible_connection": "network_cli",
-                    "ansible_network_os": "edgeos"
+            'routers': {
+                'hosts': self.routers,
+                'vars': {
+                    'ansible_user': self.USERNAME,
+                    'ansible_password': self.PASSWORD,
+                    'ansible_connection': 'network_cli',
+                    'ansible_network_os': 'edgeos'
                 }
             },
-            "container_linux": {
-                "hosts": self.container_linux,
-                "vars": {
-                    "ansible_user": "core",
-                    "ansible_ssh_private_key_file": "~/.ssh/id_rsa",
-                    "ansible_python_interpreter": "/opt/bin/python",
-                    "bin_dir": "/opt/bin",
-                    "ansible_become": "yes"
-                }
+            'pxe_server': {
+                'hosts': ['localhost'],
             },
-            "pxe_server": {
-                "hosts": ["localhost"],
+            'etcd': {
             },
-            "kube-node": {
-                'children': ['container_linux']
+            'kube-node': {
             },
-            "kube-master": {
-                'children': ['container_linux']
+            'kube-master': {
             },
-            "etcd": {
-                'children': ['container_linux']
-            },
-            "k8s-cluster": {
+            'k8s-cluster': {
                 'children': ['kube-node','kube-master','calico-rr']
             },
-            "_meta": {
-                "hostvars": self.hostvars
+            '_meta': {
+                'hostvars': self.hostvars
             }
         }
+        self.inventory['kube-node']['hosts'] = \
+            self.container_linux
+        self.inventory['kube-master']['hosts'] = \
+            self.container_linux[:3]
+        num_hosts = len(self.container_linux)
+        if not num_hosts % 2 and num_hosts < 7:
+            self.inventory['etcd']['hosts'] = \
+                self.container_linux[1:7]
+        else:
+            self.inventory['etcd']['hosts'] = \
+                self.container_linux[:7]
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     # Get the inventory.
     LocalNetworkInventory()
